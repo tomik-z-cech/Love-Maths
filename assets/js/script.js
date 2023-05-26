28cbe1cca54ba22f6f1c11a936abe891b86cc03d
tomik-z-cech <>
2023-05-26 07:12:23 +0000

    Add code to display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,22 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked Submit!");
             }else{
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+    runGame("addition");
 });
 
-function runGame(){
+function runGame(gameType){
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1, num2);
+    }else{
+        alert(`Unknown game type : ${gameType}`);
+        throw `Unknown game type : ${gameType}. Aborting!`;
+    }
 };
 
 function checkAnswer(){
@@ -33,8 +40,10 @@ function incrementWrongAnswer(){
 
 };
 
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 };
 
 function displaySubtractQuestion(){
