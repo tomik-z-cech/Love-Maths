f3aafc2bc70eddec9dd37cd1172c4a4798b33966
tomik-z-cech <>
2023-05-26 07:56:07 +0000

    Addc code to count correct and incorrect answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,8 +30,10 @@ function checkAnswer(){
     let isCorrect = userAnswer === calculatedAnswer[0];
     if (isCorrect){
         alert('Hey! You got it right! :D');
+        incrementScore();
     }else{
         alert(`Awwwwh ... You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]} !`);
+        incrementWrongAnswer();
     }
     runGame(calculatedAnswer[1]);
 };
@@ -51,11 +53,13 @@ function calculateCorrectAnswer(){
 };
 
 function incrementScore(){
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 };
 
 function incrementWrongAnswer(){
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 };
 
 function displayAdditionQuestion(operand1, operand2){
